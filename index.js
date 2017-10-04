345d03022296f4b8db0d89eeb6bf4aacb3290f2c
Antonin <antonin.hackenberg@gmail.com>
2017-10-04 18:18:27 +0200

    Run node

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const { sum, multiply } = require("./sum/")
+const sum = require("./sum/sum")
 
 console.log(">>>> Sum 3 and 6")
 console.log(sum(3, 6));
-console.log(">>>> Multiplication of 3 and 6");
-console.log(multiply(3, 6));
